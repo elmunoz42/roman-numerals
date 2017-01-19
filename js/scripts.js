4b22ac1fe52628c6a3721a9eb5afa091aaeec87d
Azure Rose Legacy and Carlos Munoz Kampff <carlos@>
2017-01-19 10:41:57 -0800

    meet third specification (adds to 99)

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,109 @@
 var numberInput;
 var numberArray = [];
 var romanArray = [];
+var romanNumeral;
 
-///// rightmost digit ////
+///// numberSplitter ////
+var numberSplitter = function(numberInput) {
+  if (numberInput < 10) {
+    numberArray.splice(0,0,"0","0","0");
+     alert(numberArray);
+  } else if (numberInput < 100) {
+    numberArray.splice(0,0,"0","0");
+      alert(numberArray);
+  } else if (numberInput < 1000) {
+    numberArray.splice(0,0,"0");
+      alert(numberArray);
+  } else {
+    alert(numberArray);
+  }
+
+}
+
+
+///// penultimateDigit ///
+
+var penultimateDigit = function(digit) {
+  digit = numberArray [2];
+  if (digit === "0") {
+    alert("second digit = 0");
+} else if (digit === "1") {
+  alert("X");
+  alert(romanArray);
+  romanArray.push("X");
+} else if (digit === "2") {
+  alert("XX");
+  romanArray.push("XX");
+  alert(romanArray);
+} else if (digit === "3") {
+  alert("XXX");
+  romanArray.push("XXX");
+  alert(romanArray);
+} else if (digit === "4") {
+  romanArray.push("IL");
+  alert(romanArray);
+} else if (digit === "5") {
+  romanArray.push("L");
+  alert(romanArray);
+} else if (digit === "6") {
+  romanArray.push("LI");
+  alert(romanArray);
+} else if (digit === "7") {
+  romanArray.push("LII");
+  alert(romanArray);
+} else if (digit === "8") {
+  romanArray.push("LIII");
+  alert(romanArray);
+} else if (digit === "9") {
+  romanArray.push("IC");
+  alert(romanArray);
+}
+  else {
+  alert("doooom!");
+}
+}
+///// rightmostDigit ////
 var rightmostDigit = function(digit) {
-  digit = numberArray [0];
+  digit = numberArray [3];
   if (digit === "0") {
     alert("first digit = 0");
+    alert(romanArray);
 
   } else if (digit === "1") {
     alert("I");
+    alert(romanArray);
     romanArray.push("I");
-  } else {
+  } else if (digit === "2") {
+    alert("II");
+    romanArray.push("II");
+    alert(romanArray);
+  } else if (digit === "3") {
+    alert("III");
+    romanArray.push("III");
+    alert(romanArray);
+  } else if (digit === "4") {
+    romanArray.push("IV");
+    alert(romanArray);
+  } else if (digit === "5") {
+    romanArray.push("V");
+    alert(romanArray);
+  } else if (digit === "6") {
+    romanArray.push("VI");
+    alert(romanArray);
+  } else if (digit === "7") {
+    romanArray.push("VII");
+    alert(romanArray);
+  } else if (digit === "8") {
+    romanArray.push("VIII");
+    alert(romanArray);
+  } else if (digit === "9") {
+    romanArray.push("IX");
+    alert(romanArray);
+  }
+    else {
     alert("doooom!");
   }
+
 }
 
 
@@ -29,13 +119,20 @@ $(document).ready(function(){
   alert("hello!");
   numberInput = prompt("input a number");
   // numberInput = numberInput / 1000;
-  alert("numberInput: " + numberInput);
+  // alert("numberInput: " + numberInput);
   numberArray = numberInput.split("");
-  alert("numberArray; " + numberArray);
-  alert("rightmostDigit" + numberArray[0]);
+  console.log("numberArray " + numberArray);
+  numberSplitter(numberInput);
+  // numberArray = numberArrayNR.reverse();
+  console.log("numberArray " + numberArray);
+
+  // alert("numberArray; " + numberArray);
+  // alert("rightmostDigit" + numberArray[0]);
   ///// rightmost digit ////
-  rightmostDigit(numberArray[0]);
-
-
+  penultimateDigit(numberArray[2]);
+  rightmostDigit(numberArray[3]);
+  alert(romanArray);
+  romanNumeral = romanArray.join("");
+  alert(romanNumeral);
 
 });
